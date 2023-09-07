70351aa1311cfd80d6e113e071b5146326de2624
Julien Vantyghem <jvantyghem@gmail.com>
2023-09-07 23:24:35 +0200

    remove unused resources and utils

--- pulpocon23_dagster/chapter_2/__init__.py
+++ pulpocon23_dagster/chapter_2/__init__.py
@@ -2,8 +2,6 @@ from dagster import Definitions, FilesystemIOManager, load_assets_from_package_m
 from dagster_aws.s3 import S3Resource
 
 from . import assets as my_assets
-from .jobs.test_postgres import introspect_table_job
-from .resources import operational_data_repository
 from .resources.partitioned_numeric_time_series_postgres_io_manager import PartitionedNumericTimeSeriesPostgresIOManager
 from .resources.postgres_resource import PostgresResource
 from .resources.s3_json_io_manager import S3JsonIOManager
@@ -33,9 +31,7 @@ partitioned_numeric_time_series_postgres_io_manager = PartitionedNumericTimeSeri
 
 definitions = Definitions(
     assets=asset_definitions,
-    jobs=[introspect_table_job],
     resources={
-        "operational_data_repository": operational_data_repository,
         "io_manager": file_system_io_manager,
         "s3_json_io_manager": s3_json_io_manager,
         "postgres": postgres_resource,
