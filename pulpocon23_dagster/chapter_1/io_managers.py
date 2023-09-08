803ad886d45d7ff3912989ba3d64380a01b0d4c9
Julien Vantyghem <jvantyghem@gmail.com>
2023-09-08 11:00:25 +0200

    fix give me five asset prefix and group

--- pulpocon23_dagster/chapter_1/io_managers.py
+++ pulpocon23_dagster/chapter_1/io_managers.py
@@ -1,6 +1,6 @@
 from dagster import asset
 
 
-@asset(io_manager_key="fs_io_manager")
+@asset(key_prefix=["examples"], group_name="io_manager", name="give_me_five_asset", io_manager_key="fs_io_manager")
 def give_me_five_asset():
     return 5
