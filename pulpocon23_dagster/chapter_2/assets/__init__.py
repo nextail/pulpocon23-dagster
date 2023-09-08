15df7c193d04b4f6d8bd1e6ee7205c431c6cf534
Julien Vantyghem <jvantyghem@gmail.com>
2023-09-08 09:02:44 +0200

    solution to assignment 6 - your way

--- pulpocon23_dagster/chapter_2/assets/__init__.py
+++ pulpocon23_dagster/chapter_2/assets/__init__.py
@@ -1,3 +1,5 @@
+from numbers import Number
+
 import pandas as pd
 from dagster import (
     AssetExecutionContext,
@@ -6,9 +8,6 @@ from dagster import (
     MarkdownMetadataValue,
     asset,
 )
-
-from ..resources.postgres_resource import PostgresResource
-from ..utils.time_series_dao import TimeSeriesDao
 
 daily_partitioning = DailyPartitionsDefinition(
     start_date="2023-08-08", end_date="2023-09-08"
@@ -31,46 +30,41 @@ def operational_data(
     return data
 
 
-# Taking persistence into our own hands, using basic dependency https://docs.dagster.io/concepts/assets/software-defined-assets#defining-basic-dependencies
-@asset(partitions_def=daily_partitioning)
-def revenue(
-    context: AssetExecutionContext,
-    operational_data: pd.DataFrame,
-    postgres: PostgresResource,
-):
+# With basic managed-loading dependency (https://docs.dagster.io/concepts/assets/software-defined-assets#defining-basic-managed-loading-dependencies)
+@asset(
+    partitions_def=daily_partitioning,
+    io_manager_key="partitioned_numeric_time_series_postgres_io_manager",
+)
+def revenue(context: AssetExecutionContext, operational_data: pd.DataFrame) -> float:
     value = float(
         operational_data["Sales"].sum()
     )  # Need to convert to float because numpy numeric types are not supported
     context.add_output_metadata({"value": value})
-    dao = TimeSeriesDao("revenue", postgres)
-    date = context.partition_key
-    dao.upsert_record(date, value)
+    return value
 
 
+# With explicit managed-loading dependency (https://docs.dagster.io/concepts/assets/software-defined-assets#defining-explicit-managed-loading-dependencies)
 @asset(
     ins={"upstream_operational_data": AssetIn(key=["operational_data"])},
     key="units_sold",  # default key (function name) can be overridden
     partitions_def=daily_partitioning,
+    io_manager_key="partitioned_numeric_time_series_postgres_io_manager",
 )
 def function_computing_units_sold(
-    context: AssetExecutionContext,
-    upstream_operational_data: pd.DataFrame,
-    postgres: PostgresResource,
-):
+    context: AssetExecutionContext, upstream_operational_data: pd.DataFrame
+) -> int:
     value = int(upstream_operational_data["Quantity"].sum())
     context.add_output_metadata({"value": value})
-    dao = TimeSeriesDao("units_sold", postgres)
-    date = context.partition_key
-    dao.upsert_record(date, value)
+    return value
 
 
-# We go back to explicitly define dependencies between assets, as well as extracting / loading
-@asset(partitions_def=daily_partitioning, deps=[revenue, function_computing_units_sold])
-def average_sales_price(context: AssetExecutionContext, postgres: PostgresResource):
-    date = context.partition_key
-    revenue = TimeSeriesDao("revenue", postgres).get_value(date)
-    units_sold = TimeSeriesDao("units_sold", postgres).get_value(date)
+@asset(
+    partitions_def=daily_partitioning,
+    io_manager_key="partitioned_numeric_time_series_postgres_io_manager",
+)
+def average_sales_price(
+    context: AssetExecutionContext, revenue: Number, units_sold: Number
+) -> float:
     value = float(revenue / units_sold)
     context.add_output_metadata({"value": value})
-    dao = TimeSeriesDao("average_sales_price", postgres)
-    dao.upsert_record(date, value)
+    return value
